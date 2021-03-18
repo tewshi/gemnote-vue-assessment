6ff7da7f03de0667e6ee5f9757aba23c0f6ffc05
Joseph Ojoko <entewshi@gmail.com>
2021-03-18 20:56:18 +0100

    refactor router, add content to app' state

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      firstName: "Ashley",
+      lastName: "Wong",
+    },
+    inventory: {
+      ordersThisMonth: 2736,
+      averageMonthlyShipments: 364,
+      inventoryTotal: 400,
+    },
+  },
   mutations: {},
   actions: {},
+  getters: {
+    username({ user: { firstName, lastName } }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
   modules: {},
 });
