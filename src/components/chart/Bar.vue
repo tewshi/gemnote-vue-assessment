2e6ec79841ecd9d334f3fbe78d2fb7a7e4143b7e
Joseph Ojoko <entewshi@gmail.com>
2021-03-18 20:54:55 +0100

    chore: add bar and pie chart components

---
+++ src/components/chart/Bar.vue
@@ -0,0 +1,20 @@
+<script>
+import { Bar } from "vue-chartjs";
+export default {
+  name: "BarChart",
+  extends: Bar,
+  props: {
+    chartdata: {
+      type: Object,
+      default: () => ({}),
+    },
+    options: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  },
+};
+</script>
