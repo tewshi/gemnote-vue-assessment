9adc1b77581c9e8e0b2b7ff4af6eb677213d8511
Joseph Ojoko <entewshi@gmail.com>
2021-03-18 19:41:04 +0100

    chore: import tailwindcss, use src alias on imports

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import App from "@/App.vue";
+import "@/registerServiceWorker";
+import router from "@/router";
+import store from "@/store";
+
+import "tailwindcss/tailwind.css";
 
 Vue.config.productionTip = false;
 
